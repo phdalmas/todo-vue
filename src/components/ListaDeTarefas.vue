83eb184383e88607024e1dd1bff24e17e2abacb8
Pedro Dalmas <phdalforno@gmail.com>
2023-10-02 15:35:37 -0300

    commit componentes

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const props = defineProps(['tarefas']);
+</script>
+
+<template>
+    <div class="container">
+        <ul class="list-group mt-4">
+            <li class="list-group-item" v-for="tarefa in props.tarefas">
+                <input @change="evento => tarefa.finzalida = evento.target.checked" :checked="tarefa.finzalida" :id="tarefa.titulo" type="checkbox">
+                <label :class="{done: tarefa.finzalida}" class="ms-3" :for="tarefa.titulo">
+                    {{tarefa.titulo}}
+                </label>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .done{
+        text-decoration: line-through;
+    }
+</style>
